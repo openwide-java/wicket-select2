ce1bc9344a58f13456bff993abbc69cb8094081d
Guillaume Smet <guillaume.smet@gmail.com>
2013-03-01 15:33:03 +0100

    update select2 to current master as of march 1st

--- src/main/java/org/retzlaff/select2/resource/select2.js
+++ src/main/java/org/retzlaff/select2/resource/select2.js
@@ -1664,6 +1664,16 @@ the specific language governing permissions and limitations under the Apache Lic
                 }
             }));
 
+            this.search.bind("blur", this.bind(function(e) {
+                // a workaround for chrome to keep the search field focussed when the scroll bar is used to scroll the dropdown.
+                // without this the search field loses focus which is annoying
+                if (document.activeElement === this.body().get(0)) {
+                    window.setTimeout(this.bind(function() {
+                        this.search.focus();
+                    }), 0);
+                }
+            }));
+
             this.focusser.bind("keydown", this.bind(function (e) {
                 if (!this.enabled) return;
 
@@ -1936,7 +1946,7 @@ the specific language governing permissions and limitations under the Apache Lic
                 this.select
                     .val(val)
                     .find(":selected").each2(function (i, elm) {
-                        data = {id: elm.attr("value"), text: elm.text()};
+                        data = {id: elm.attr("value"), text: elm.text(), element: elm.get(0)};
                         return false;
                     });
                 this.updateSelection(data);
